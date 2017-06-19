a4b0386e0e7f4dd69bc97359baf85c76f145623a
Mohammed Yaseen <myy233@nyu.edu>
2017-06-19 19:19:21 -0400

    Setting up components

---
+++ src/Components/Webrtc.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Webrtc extends Component {
+  render() {
+    return (
+      <div className="App">
+        Webrtc
+  
+      </div>
+    );
+  }
+}
+
+export default Webrtc;
